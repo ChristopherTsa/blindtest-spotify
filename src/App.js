e0f67c48271325a597a99df0c0fde0790f26c6a4
Christopher TSA-KWET-SHIN <christopher.tsa@ensta-paris.fr>
2021-11-15 20:09:43 +0100

    Button

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import loading from "./loading.svg"
 import styles from "./App.module.css"
 import Button from "./Button"
 import { shuffleArray, getRandomNumber } from "./utils"
+import { useState } from "react"
 
 // Get token from https://developer.spotify.com/console/get-current-user-saved-tracks/
 const apiToken = "<<Copiez le token de Spotify ici>>"
 
 const App = () => {
+  const [text, setText] = useState("")
   return (
     <div className={styles.app}>
       <header className={styles.appHeader}>
@@ -18,6 +20,8 @@ const App = () => {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <button onClick={() => setText("Cliqué !")}>Cliquez moi !</button>
+        <p>{text}</p>
         <a className={styles.appLink} href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
         </a>
